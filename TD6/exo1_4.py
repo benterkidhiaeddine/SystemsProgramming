5676a57462626d8e0139723f0eb40465b700afa2
benterkidhiaeddine <benterki.dhiaeddine@gmail.com>
2026-04-11 14:27:14 +0200

    Ajouter une explication par rapport à l'héritage des descripteurs

--- TD6/exo1_4.py
+++ TD6/exo1_4.py
@@ -1,4 +1,5 @@
-import os, sys
+import os
+import sys
 
 fd = os.open("toto.txt", os.O_RDONLY)
 pid = os.fork()
@@ -9,3 +10,6 @@ os.wait()
 c = os.read(fd, 1)
 print(c)
 sys.exit(0)
+
+# Le programme va afficher 'z' parcque le fils hérite le déscripteur de fichier du père , et avec ce dernier
+# il hérite aussi la position pointeur dans le fichier
