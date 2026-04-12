f4ed29d66485d983dba1ff208980a0b4a75e9be6
benterkidhiaeddine <benterki.dhiaeddine@gmail.com>
2026-04-12 15:46:46 +0200

    Correct TD7 sockets

--- TD7/client.py
+++ TD7/client.py
@@ -1,26 +1,36 @@
-import socket, sys, os
+import os
+import socket
+import sys
 
 MAXBYTES = 4096
 host = sys.argv[1] if len(sys.argv) > 1 else "127.0.0.1"
 port = sys.argv[2] if len(sys.argv) > 2 else "2000"
 
 sockadress = (host, int(port))
+
+# Create a client socket IPv4 and TCP
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Connect the client socket to the server socket
 client_socket.connect(
     sockadress
 )  # Cette appel est bloquante, elle retourne seulement si la connexion est établie
 print("Connected to server: ", sockadress)
 while True:
+    # Read lines from stdin
     line = os.read(0, MAXBYTES)
     if len(line) == 0:
         client_socket.shutdown(socket.SHUT_WR)
         break
-
+    # Send the line to the server
     client_socket.send(line)
 
+    # Recieve data from the server
     data = client_socket.recv(MAXBYTES)
     if len(data) == 0:
         break
+
+    # Write it to stdout
     os.write(1, data)
 
 client_socket.close()
--- TD7/server2.py
+++ TD7/server2.py
@@ -1,4 +1,5 @@
-import select, socket
+import select
+import socket
 import sys
 
 HOST = "127.0.0.1"  # or 'localhost' or '' - Standard loopback interface address
@@ -7,29 +8,36 @@ MAXBYTES = 4096
 serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 serversocket.bind((HOST, PORT))
 serversocket.listen()
-fdlist = [serversocket, 0]
 
 
-while len(fdlist) > 0:
-    (readable, _, _) = select.select(fdlist, [], [])
+# Sockets objects are treated like file descriptors , that's why we put the
+# stdin file descriptor in the socket list
+socketlist = [serversocket, sys.stdin]
+
+running = True
+while running:
+    # select basicaly selects the file descriptors that are ready for reading , writing or for whome some exceptional
+    # condition appliy
+    # In our case we will only use the readable list to accept client sockets
+    (readable, _, _) = select.select(socketlist, [], [])
     for fd in readable:
         if fd == serversocket:  # serversocket receives a connection
             (clientsocket, (addr, port)) = fd.accept()
             print("connection from:", addr, port)
-            fdlist.append(clientsocket)
-        if fd == 0:  # keyboard input
-            data = input()
-            for fd in fdlist:
-                if fd != serversocket and fd != 0:  # If fd is a client socket
-                    fd.shutdown(socket.SHUT_WR)
-                serversocket.close()
-                sys.exit(0)
+            socketlist.append(clientsocket)
+        elif fd == sys.stdin:  # keyboard input
+            _ = sys.stdin.readline()
+            running = False
         else:  # data is sent from given client
             data = fd.recv(MAXBYTES)
             if len(data) > 0:
                 fd.sendall(data)
             else:  # client has disconnected
-                fdlist.remove(fd)
+                socketlist.remove(fd)
                 fd.close()
+# Close all client connections
+
+for c in socketlist:
+    c.close()
 
 serversocket.close()
