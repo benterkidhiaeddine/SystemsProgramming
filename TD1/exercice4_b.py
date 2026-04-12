bdc0ee6808df3bb992c7d73e1df9bf434de67a9c
benterkidhiaeddine <benterki.dhiaeddine@gmail.com>
2026-04-12 17:22:29 +0200

    fix fork exercise

--- TD1/exercice4_b.py
+++ TD1/exercice4_b.py
@@ -1,31 +1,34 @@
 """
-Écrire un programme qui réalise une chaîne de n sous-processus, où n est passée en paramètre de 
+Écrire un programme qui réalise une chaîne de n sous-processus, où n est passée en paramètre de
 lexécution de la commande (par exemple, n = 3 sur la figure ci-dessus). Faire
 imprimer le numéro de chaque processus et celui de son père. Même question avec la struc￾ture en arbre.
 """
-import os ,sys
+
+import os
+import sys
+
 
 def arbre_processus(n):
     for i in range(n):
         try:
             fork_result = os.fork()
-        except OSError as e:
+        except OSError:
             exit(1)
-        if fork_result == 0:#child
+        if fork_result == 0:  # fils
             print(f"le pid de père est {os.getppid()} et mon pid est {os.getpid()}")
-        else:
-            exit(0)
-        
-            
-
+        else:  # père meurt
+            sys.exit(0)
 
 
 if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print(f"Usage: {sys.argv[0]} <number_of_processes: int>")
+        sys.exit(1)
+
     n = int(sys.argv[1])
-    l = len(sys.argv)
-    if l != 2:
-        print(f"Usage exercice4_a n")
-        exit(1)
+
+    # Le père affiche d'abbord
     print(f"le pid de père est {os.getppid()} et mon pid est {os.getpid()}")
+
     arbre_processus(n)
     exit(0)
