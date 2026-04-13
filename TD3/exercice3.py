8498a4e1fa53ec9e7d8d8404525b3d660d47b765
benterkidhiaeddine <benterki.dhiaeddine@gmail.com>
2026-04-13 07:53:28 +0200

    add corrections to exercice 3

--- TD3/exercice3.py
+++ TD3/exercice3.py
@@ -1,5 +1,6 @@
 #! /usr/bin/env python3
-import os , sys
+import os
+import sys
 
 cmd1 = "who"
 args1 = ["who"]
@@ -10,22 +11,28 @@ args2 = ["pwd"]
 cmd3 = "ls"
 args3 = ["ls", "-l"]
 
+
 def execute_cmd_and_wait(cmd, args):
     try:
         fork_res = os.fork()
     except OSError as e:
         print(f"Une erreur est survenue lors du fork : {e}")
+        sys.exit(1)
 
-    if fork_res == 0:  
+    if fork_res == 0:
         try:
             os.execvp(cmd, args)
-        except OSError as e:
-            print(f"Une erreur est survenue lors de l'éxecution du process {cmd} {args} : erreur:  {e}")
+        except (OSError, PermissionError, FileNotFoundError) as e:
+            print(
+                f"Une erreur est survenue lors de l'éxecution du process {cmd} {args} : erreur:  {e}"
+            )
             sys.exit(1)
-    try: 
+    try:
         os.wait()
     except OSError:
-        print("Il y a aucun processu à attendre") 
+        print("Il y a aucun processu à attendre")
+        pass
+
 
 # Create child process to execute
 execute_cmd_and_wait(cmd1, args1)
